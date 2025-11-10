b118fc39b86696189cd4cf66d70cf603691c217f
Mattéo <matteo.spindler17@gmail.com>
2025-11-10 18:35:19 +0100

    Refactor vertex management in class module: replace t_tarjan_vertex with t_vertex and update related functions
    Signed-off-by: Mattéo <matteo.spindler17@gmail.com>

--- class.c
+++ class.c
@@ -14,28 +14,36 @@ t_class* createClass(const char *name) {
     return class;
 }
 
-void addVertexToClass(t_class *class, t_tarjan_vertex *vertex) {
-    if (class == NULL || vertex == NULL) return;
-    vertex->next = class->vertexes;
-    class->vertexes = vertex;
+void addVertexToClass(t_class *class, int value) {
+    if (class == NULL) return;
+
+    t_vertex *new_vertex = malloc(sizeof(t_vertex));
+    if (new_vertex == NULL) {
+        perror("addVertexToClass: allocation failed");
+        return;
+    }
+
+    new_vertex->value = value;
+    new_vertex->next = class->vertexes;
+    class->vertexes = new_vertex;
 }
 
 void displayClass(t_class *class) {
     if (class == NULL) return;
-    printf("Class %s:\n", class->name);
-    t_tarjan_vertex *curr = class->vertexes;
+    printf("Class %s: ", class->name);
+
+    t_vertex *curr = class->vertexes;
     while (curr != NULL) {
-        displayTarjanVertex(*curr);
-        printf("->");
+        printf("%d -> ", curr->value);
         curr = curr->next;
     }
     printf("NULL\n");
 }
 
-void freeVertexes(t_tarjan_vertex* vertex) {
+void freeVertexes(t_vertex *vertex) {
     if (vertex == NULL) return;
     freeVertexes(vertex->next);
-    freeTarjanVertex(vertex);
+    free(vertex);
 }
 
 void freeClass(t_class *class) {
@@ -43,4 +51,3 @@ void freeClass(t_class *class) {
     freeVertexes(class->vertexes);
     free(class);
 }
-
--- class.h
+++ class.h
@@ -1,18 +1,23 @@
 #ifndef CLASS_H
 #define CLASS_H
 
-#include "tarjan_vertex.h"
+#include <stdlib.h>
+
+typedef struct s_vertex {
+    int value;
+    struct s_vertex *next;
+} t_vertex;
 
 typedef struct s_class {
     char name[10];
-    t_tarjan_vertex *vertexes;
+    t_vertex *vertexes;
     struct s_class *next;
 } t_class;
 
-t_class* createClass(const char *);
-void addVertexToClass(t_class *, t_tarjan_vertex *);
-void displayClass(t_class *);
-void freeVertexes(t_tarjan_vertex*);
-void freeClass(t_class *);
+t_class* createClass(const char *name);
+void addVertexToClass(t_class *class, int value);
+void displayClass(t_class *class);
+void freeVertexes(t_vertex *vertex);
+void freeClass(t_class *class);
 
 #endif //CLASS_H
