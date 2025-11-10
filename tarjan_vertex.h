915922e1045cd5b3670e48806fb5093119415342
mathas42 <mathias.leroy@efrei.net>
2025-11-10 17:14:59 +0100

    tarjan_vertex.h

--- tarjan_vertex.h
+++ tarjan_vertex.h
@@ -1,8 +1,20 @@
-//
-// Created by matteo on 10/11/2025.
-//
 
 #ifndef TARJAN_VERTEX_H
 #define TARJAN_VERTEX_H
+#include <stdlib.h>
+
+
+struct s_tarjan_vertex {
+    int id;
+    int num;
+    int num_accessible;
+    int in_pile;
+    struct s_tarjan_vertex* next;
+};
+typedef struct s_tarjan_vertex t_tarjan_vertex;
+
+t_tarjan_vertex* createTarjanVertex(int, int, int, int);
+void displayTarjanVertex(t_tarjan_vertex);
+void freeTarjanVertex(t_tarjan_vertex*);
 
 #endif //TARJAN_VERTEX_H
