21bbeefe0eb61822307487a3a0955fdbf571365f
mathas42 <mathias.leroy@efrei.net>
2025-11-10 17:42:38 +0100

    tarjan_vertex.c

--- tarjan_vertex.c
+++ tarjan_vertex.c
@@ -1,5 +1,30 @@
-//
-// Created by matteo on 10/11/2025.
-//
+
 
 #include "tarjan_vertex.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+t_tarjan_vertex * createTarjanVertex(int id, int num, int num_accessible, int in_pile) {
+    t_tarjan_vertex * tarjan_vertex;
+    tarjan_vertex = (t_tarjan_vertex *)malloc(sizeof(t_tarjan_vertex));
+    if (tarjan_vertex == NULL) {
+        //perror
+        return NULL;
+    }
+    tarjan_vertex->id = id;
+    tarjan_vertex->num = num;
+    tarjan_vertex->id = num_accessible;
+    tarjan_vertex->in_pile = in_pile;
+    tarjan_vertex->next = NULL;
+    return tarjan_vertex;
+}
+
+void displayTarjanVertex(t_tarjan_vertex tarjan_vertex) {
+    printf("(%d, %d, %d, %d )", tarjan_vertex.id, tarjan_vertex.num,tarjan_vertex.num_accessible, tarjan_vertex.in_pile);
+}
+
+void freeTarjanVertex(t_tarjan_vertex* tarjan_vertex) {
+    if (tarjan_vertex == NULL) return;
+    free(tarjan_vertex);
+}
