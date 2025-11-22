330c435f59588ebcf7c26066f6477ca1e41aaf8c
Mattéo <matteo.spindler17@gmail.com>
2025-11-22 17:31:48 +0100

    Refactor list operations: update addCell and freeList to return status codes and enhance error handling; improve documentation in list.h
    Signed-off-by: Mattéo <matteo.spindler17@gmail.com>

--- list.h
+++ list.h
@@ -3,13 +3,53 @@
 
 #include "cell.h"
 
+/**
+ * @file list.h
+ * @brief Linked list structure and operations
+ */
+
+/**
+ * @struct s_list
+ * @brief Represents a singly linked list of cells
+ * @param head Pointer to the first cell in the list
+ */
 typedef struct s_list {
     t_cell* head;
 }t_list;
 
+/**
+ * @brief Creates an empty list
+ * @return An empty list with head set to NULL
+ */
 t_list createEmptyList();
-void displayList(t_list);
-void addCell(t_list*, int, double);
-void freeList(t_list*);
-double sumValues(t_list list);
+
+/**
+ * @brief Displays all cells in the list
+ * @param list The list to display
+ */
+void displayList(t_list list);
+
+/**
+ * @brief Adds a new cell at the head of the list
+ * @param list Pointer to the list
+ * @param vertex The vertex identifier for the new cell
+ * @param weight The weight value for the new cell
+ * @return 1 on success, -1 on failure (NULL list or allocation error)
+ */
+int addCell(t_list* list, int vertex, double weight);
+
+/**
+ * @brief Frees all cells in the list
+ * @param list Pointer to the list to free
+ * @return 1 on success, -1 if list is NULL
+ */
+int freeList(t_list* list);
+
+/**
+ * @brief Calculates the sum of all weights in the list
+ * @param list The list to sum
+ * @return The sum of all weight values
+ */
+double sumListValues(t_list list);
+
 #endif //LIST_H
