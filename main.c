69c143ade43cad471eb0bce36a3fc2b2da7f6ebb
Mattéo <matteo.spindler17@gmail.com>
2025-11-23 17:44:13 +0100

    Enhance main function: import graph, display and export to Mermaid, implement Tarjan and Hasse diagram functionalities, and add state checks
    Signed-off-by: Mattéo <matteo.spindler17@gmail.com>

--- main.c
+++ main.c
@@ -6,8 +6,30 @@
 #include "tests.h"
 #include <string.h>
 #include "matrix.h"
+#include "tarjan.h"
 
 int main(void) {
+
+    t_graph graph = importGraphFromFile("../test_bench/exemple3.txt");
+    if (graph.size == 0) {
+        fprintf(stderr, "Failed to import graph from file.\n");
+        return -1;
+    }
+    displayGraph(graph);
+    exportGraphToMermaidFile(graph, "../results/exemple3_mermaid.mmd");
+    t_partition *partition = tarjan(graph);
+    displayPartition(partition);
+    t_hasse_diagram hasse = createHasseDiagram(graph);
+    displayHasseDiagram(hasse);
+    exportHasseDiagramToMermaidFile(hasse, "../results/exemple3_hasse_mermaid.mmd");
+    printf("Irreductible ? : %d\n", isIrreductible(hasse));
+    printf("Persistant A ? : %d\n", isPersistantClass(hasse, 1));
+    printf("Persistant C ? : %d\n", isPersistantClass(hasse, 3));
+    printf("Absorbing ? : %d\n", isAbsorbingState(hasse, 4, graph.size));
+
+
+
+    /*
     // Enregistrer les tests du projet puis exécuter tous les tests.
     register_project_tests();
     int failures = run_all_tests();
@@ -15,4 +37,5 @@ int main(void) {
     // Retourner le nombre d'échecs comme code de sortie (0 = tout OK)
     return failures;
     return 0;
+    */
 }
