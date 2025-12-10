623ef95b337f92185da70172789a499813236d18
Mattéo <matteo.spindler17@gmail.com>
2025-12-10 18:35:10 +0100

    Add Mermaid files for graph and Hasse diagram representations
    Signed-off-by: Mattéo <matteo.spindler17@gmail.com>

--- main.c
+++ main.c
@@ -8,47 +8,32 @@
 
 int main(void) {
     /* à modifier selon où est situé votre fichier matrice.txt */
-    const char* matrix_file = "C:/Users/matte/OneDrive - Efrei/Efrei/2025-2026/SM301 - Probabilité/Projet/Probability_SM301_Projet_Matrice.txt";
-    const char* mermaid_file = "../results/exemple_graph.mmd";
+    const char* matrix_file = "../data/matrice_m.txt";
+    const char* mermaid_file = "../results/graph_m.mmd";
+    const char* hasse_file = "../results/hasse_m.mmd";
 
     /* Importation du graphe */
     t_graph graph = importGraphFromFile(matrix_file);
 
-    /* Exportation du graphe au format Mermaid */
     exportGraphToMermaidFile(graph, mermaid_file);
+    t_hasse_diagram hasse = createHasseDiagram(graph);
+    exportHasseDiagramToMermaidFile(hasse, hasse_file);
 
-    /* Transformation du graphe en matrice d'adjacence */
+    /*
     t_matrix matrix;
     createMatrixFromGraph(graph, &matrix);
-    /*
-        états : 2, 5, 12, 21 et 25
-        0.12, 0.18, 0.25, 0.20, 0.25 → somme = 1.00
-            setMatrixValue(&distribution, 0, 1, 0.12);
-            setMatrixValue(&distribution, 0, 4, 0.18);
-            setMatrixValue(&distribution, 0, 11, 0.25);
-            setMatrixValue(&distribution, 0, 20, 0.20);
-            setMatrixValue(&distribution, 0, 24, 0.25);
-
-        0.10, 0.30, 0.15, 0.25, 0.20 → somme = 1.00
-            setMatrixValue(&distribution, 0, 1, 0.10);
-            setMatrixValue(&distribution, 0, 4, 0.30);
-            setMatrixValue(&distribution, 0, 11, 0.15);
-            setMatrixValue(&distribution, 0, 20, 0.25);
-            setMatrixValue(&distribution, 0, 24, 0.20);
-
-        0.22, 0.18, 0.20, 0.15, 0.25 → somme = 1.00
+    displayMatrix(matrix);
+    int* array = NULL;
+    array = (int*)malloc(sizeof(int)*6);
+    array[0] = 1;
+    array[1] = 4;
+    array[2] = 11;
+    array[3] = 18;
+    array[4] = 26;
+    array[5] = 27;
+    t_matrix sub_matrix = buildSubMatrixFromVertices(matrix, array, 6);
+    displayMatrix(sub_matrix);
     */
-
-    /* Création d'une distribution initiale */
-    t_matrix distribution = createMatrix(1, matrix.cols);
-    setMatrixValue(&distribution, 0, 1, 0.22);
-    setMatrixValue(&distribution, 0, 4, 0.18);
-    setMatrixValue(&distribution, 0, 11, 0.20);
-    setMatrixValue(&distribution, 0, 20, 0.15);
-    setMatrixValue(&distribution, 0, 24, 0.25);
-
-    /* Calcul et affichage de la distribution limite */
-    displayConvergedMatrixPower(matrix, distribution, 0.0001, 2000);
 
     return 1;
 }
